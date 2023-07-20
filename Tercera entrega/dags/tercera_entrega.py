7ca4a33c0cc92bee4da76d310c175f3c872736dc
Cokepoma <99054223+Cokepoma@users.noreply.github.com>
2023-07-20 22:45:38 +0200

    tercera_entrega.py

--- Tercera entrega/dags/tercera_entrega.py
+++ Tercera entrega/dags/tercera_entrega.py
@@ -8,16 +8,6 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.models import Variable
 from airflow.operators.email_operator import EmailOperator
 
-
-"""USUARIO = 'jorgepons90_coderhouse'
-CONTRASEÑA = 'aJSb9Nf6G6'
-ENDPOINT = 'data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com'
-PUERTO = 5439
-BASE_DE_DATOS = 'data-engineer-database'
-connection_string = "postgresql://<usuario>:<contraseña>@<endpoint>:<puerto>/<nombre_base_de_datos>"
-
-API_KEY='ldck7dJTzob1DJaYv28PAM45nyFYojrB'"""
-
 USUARIO = Variable.get("USUARIO")
 CONTRASEÑA = Variable.get("CONTRASEÑA")
 ENDPOINT = Variable.get("ENDPOINT")
